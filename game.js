20ec316e811946d3fa2ced083a484589915681bd
Matt Tomlin <mmtomlin@gmail.com>
2020-05-08 15:46:48 +0100

    split cards out into json file

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// Game 'class'
 function Game() {
   this.startGame = function () {
     this.deck = new Deck();
@@ -6,30 +7,12 @@ function Game() {
   };
 }
 
+// Deck 'class'
 function Deck() {
   this.cards = [];
   this.discarded = [];
 
-  k = 0;
-  for (let i = 0; i < PROPCARDS.length; i++) {
-    let rev = null;
-    for (let j = 0; j < PROPCARDS[i].numberof; j++) {
-      if (typeof PROPCARDS[i].reverseColour === "string") {
-        rev = PROPCARDS[i].reverseColour;
-      } else {
-        rev = null;
-      }
-
-      this.cards[k] = new PropCard(
-        PROPCARDS[i].colour,
-        j,
-        PROPCARDS[i].value,
-        true,
-        rev
-      );
-      k++;
-    }
-  }
+  // Generate property cards:
 
   this.shuffle = function () {
     shuffle(this.cards);
@@ -49,6 +32,7 @@ function Deck() {
   };
 }
 
+// Fisher-Yates Shuffle agorithm
 function shuffle(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
@@ -60,16 +44,15 @@ function shuffle(a) {
   return a;
 }
 
-function PropCard(colour, id, value, isprop, reverse, power = null) {
+// Card 'class'
+function Card(cardType, colour, id, value, power, reverse, image) {
+  this.cardType = cardType;
   this.colour = colour;
-  this.value = value;
   this.id = id;
-  if (reverse === null) {
-    this.name = "prop-" + colour + "-" + (id + 1).toString();
-  } else {
-    this.name = "prop-wildcard-" + colour + "-" + reverse;
-  }
+  this.value = value;
+  this.power = power;
   this.reverse = reverse;
+  this.image = image;
 
   this.flip = function () {
     if (this.reverse !== null) {
@@ -78,218 +61,21 @@ function PropCard(colour, id, value, isprop, reverse, power = null) {
       this.colour = l;
     }
   };
+
+  this.isprop = function () {
+    if (
+      this.cardType === "Prop" ||
+      this.cardType === "PropWC" ||
+      this.cardType === "PropAny"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
 
-const PROPCARDS = [
-  // Main property cards
-  {
-    colour: "brown",
-    numberof: 2,
-    value: 1,
-  },
-  {
-    colour: "dblue",
-    numberof: 2,
-    value: 4,
-  },
-  {
-    colour: "green",
-    numberof: 3,
-    value: 4,
-  },
-  {
-    colour: "lblue",
-    numberof: 3,
-    value: 1,
-  },
-  {
-    colour: "orange",
-    numberof: 3,
-    value: 2,
-  },
-  {
-    colour: "purple",
-    numberof: 3,
-    value: 2,
-  },
-  {
-    colour: "rail",
-    numberof: 4,
-    value: 2,
-  },
-  {
-    colour: "red",
-    numberof: 3,
-    value: 3,
-  },
-  {
-    colour: "utility",
-    numberof: 2,
-    value: 2,
-  },
-  {
-    colour: "yellow",
-    numberof: 3,
-    value: 3,
-  },
-  // Wildcards
-  {
-    colour: "dblue",
-    numberof: 1,
-    value: 4,
-    reverseColour: "green",
-  },
-  {
-    colour: "lblue",
-    numberof: 1,
-    value: 1,
-    reverseColour: "brown",
-  },
-  {
-    colour: "orange",
-    numberof: 1,
-    value: 2,
-    reverseColour: "purple",
-  },
-  {
-    colour: "green",
-    numerof: 1,
-    value: 4,
-    reverseColour: "rail",
-  },
-  {
-    colour: "lblue",
-    numberof: 1,
-    value: 4,
-    reverseColour: "rail",
-  },
-  {
-    colour: "utility",
-    numberof: 1,
-    value: 4,
-    reverseColour: "rail",
-  },
-  {
-    colour: "yellow",
-    numberof: 1,
-    value: 3,
-    reverseColour: "red",
-  },
-  // Multi wildcard
-  {
-    colour: "any",
-    numberof: 2,
-    value: 0,
-  }
-];
-
-const CASHCARDS = [
-  {
-    value: 10,
-    numberof: 1
-  },
-  {
-    value: 5,
-    numerof: 2
-  },
-  {
-    value: 4,
-    numberof: 3
-  },
-  {
-    value: 3,
-    numberof: 3
-  },
-  {
-    value: 2,
-    numberof: 5
-  },
-  {
-    value: 1,
-    numberof: 6
-  }
-]
-
-const RENTCARDS = [
-  {
-    colours = "any",
-    numberof = 3
-  },
-  {
-    colours = ["dblue", "green"],
-    numberof = 2
-  },
-  {
-    colours = ["brown","lblue"],
-    numberof = 2
-  },
-  {
-    colours = ["purple", "orange"],
-    numberof = 2
-  },
-  {
-    colours = ["rail", "utility"],
-    numberof = 2
-  },
-  {
-    colours = ["red", "yellow"],
-    numberof = 2
-  }
-]
-
-const POWERCARDS = [
-  {
-    power: dealBreaker,
-    value: 5,
-    numberof: 2
-  },
-  {
-    power: debtCollector,
-    value: 3,
-    numberof: 3
-  },
-  {
-    power: doubleTheRent,
-    value: 1,
-    numberof: 1
-  },
-  {
-    power: forcedDeal,
-    value: 3,
-    numberof: 3
-  },
-  {
-    power: justSayNo,
-    value: 4, 
-    numberof: 3
-  },
-  {
-    power: itsMyBirthday,
-    value: 2,
-    numberof: 3
-  },
-  {
-    power: slyDeal,
-    value: 3,
-    numberof: 3
-  },
-  {
-    power: passGo,
-    value: 1,
-    numberof: 10
-  },
-  {
-    power: hotel,
-    value: 4,
-    numberof: 3
-  },
-  {
-    power: house,
-    value: 3,
-    numberof: 3
-  }
-]
-
+// Rent amounts:
 const RENTS = {
   brown: [1, 2],
   dblue: [3, 8],
@@ -303,5 +89,6 @@ const RENTS = {
   yellow: [2, 4, 6],
 };
 
+// Init game:
 var game = new Game();
 game.startGame();
